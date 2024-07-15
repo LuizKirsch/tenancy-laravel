4de0eadc944faafc8111692b9f0013218d322eb6
lluiziull <luizkirsch0@gmail.com>
2024-07-15 15:12:43 -0300

    att

--- routes/tenant.php
+++ routes/tenant.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+use App\Livewire\Counter;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,5 @@ Route::middleware([
     Route::get('/', function () {
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
+    Route::get('/counter', Counter::class);
 });
